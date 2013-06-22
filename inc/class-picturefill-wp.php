b00bf9c18c6781f3769ef1a4da39d86a0f391e05
Kyle Reicks <reicks.kyle@gmail.com>
2013-06-22 11:13:04 -0500

    Separate out caching to its own method.

--- inc/class-picturefill-wp.php
+++ inc/class-picturefill-wp.php
@@ -37,7 +37,8 @@ if(!class_exists('Picturefill_WP')){
     private function __construct(){
       add_action('wp_enqueue_scripts', array($this, 'register_picturefill_scripts'));
       add_action('init', array($this, 'add_image_sizes'));
-      add_filter('the_content', array($this, 'replace_images'), 11);
+//      add_filter('the_content', array($this, 'replace_images'), 11);
+      add_filter('the_content', array($this, 'cache_picturefill_output'), 11);
     }
 
     // Filter and action methods
@@ -45,39 +46,39 @@ if(!class_exists('Picturefill_WP')){
       wp_register_script('picturefill', PICTUREFILL_WP_URL . 'js/libs/picturefill.min.js', array(), PICTUREFILL_WP_VERSION, true);
     }
 
-    public function replace_images($html){
-      $has_pictures = false;
+    public function cache_picturefill_output($html, $content_type = 'the_content'){
+      global $wp_scripts;
       $post_id = get_the_ID();
       $cache_duration = 86400;
-      $cached_output = get_transient('picturefill_wp_output_' . $post_id);
+      $cached_output = get_transient('picturefill_wp_' . $content_type . '_output_' . $post_id);
       if(!empty($cached_output) && (get_option('_transient_timeout_picturefill_wp_output_' . $post_id) - $cache_duration) > get_the_modified_time('U')){
         wp_enqueue_script('picturefill');
         return $cached_output;
       }else{
-        require_once(PICTUREFILL_WP_PATH . 'inc/class-model-picturefill-wp.php');
-        $DOMDocument = Model_Picturefill_WP::get_DOMDocument();
-        $images = Model_Picturefill_WP::get_images($DOMDocument, $html);
-        if($images->length > 0){
-          $has_pictures = true;
-          require_once(PICTUREFILL_WP_PATH . 'inc/class-view-picturefill-wp.php');
-          wp_enqueue_script('picturefill');
-          $html = View_Picturefill_WP::standardize_img_tags($html);
-          foreach($images as $image){
-            $model_picturefill_wp = new Model_Picturefill_WP($DOMDocument, $image);
-            $view_picturefill_wp = new View_Picturefill_WP($model_picturefill_wp);
-
-            $html = str_replace($view_picturefill_wp->get_original_image(), $view_picturefill_wp->render_template('picture'), $html);
-          }
+        $output = $this->replace_images($html);
+        if(in_array('picturefill', $wp_scripts->queue)){
+          set_transient('picturefill_wp_' . $content_type . '_output_' . $post_id, $output, $cache_duration);
         }
-        if($has_pictures){
-          $output = apply_filters('picturefill_wp_the_content_output', $html);
-          set_transient('picturefill_wp_output_' . $post_id, $output, $cache_duration);
+        return $output;
+      }
+    }
 
-          return $output;
-        }else{
-          return apply_filters('picturefill_wp_the_content_output', $html);
+    public function replace_images($html){
+      require_once(PICTUREFILL_WP_PATH . 'inc/class-model-picturefill-wp.php');
+      $DOMDocument = Model_Picturefill_WP::get_DOMDocument();
+      $images = Model_Picturefill_WP::get_images($DOMDocument, $html);
+      if($images->length > 0){
+        require_once(PICTUREFILL_WP_PATH . 'inc/class-view-picturefill-wp.php');
+        wp_enqueue_script('picturefill');
+        $html = View_Picturefill_WP::standardize_img_tags($html);
+        foreach($images as $image){
+          $model_picturefill_wp = new Model_Picturefill_WP($DOMDocument, $image);
+          $view_picturefill_wp = new View_Picturefill_WP($model_picturefill_wp);
+
+          $html = str_replace($view_picturefill_wp->get_original_image(), $view_picturefill_wp->render_template('picture'), $html);
         }
       }
+      return apply_filters('picturefill_wp_replace_images_output', $html);
     }
 
     public function add_image_sizes(){
