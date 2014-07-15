ce5d49fe2372daa12fe8fd17c06f3ef21b9e05c8
Kyle Reicks <reicks.kyle@gmail.com>
2014-07-15 15:18:20 -0500

    use image attributes in view re: issue #36

--- inc/templates/image-template.php
+++ inc/templates/image-template.php
@@ -1,6 +1,6 @@
 <img<?php
   echo $view->get_image_attribute_string();
-  if(false !== $view->model->get_image_attributes()['attachment_id']){
+  if(false !== $view->image_attributes['attachment_id']){
     echo $view->get_sizes();
     echo ' srcset="' . $view->format_srcset($template_data) . '"';
   }
