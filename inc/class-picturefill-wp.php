15ed7ff4260d155985359d393b9b6323378f6d53
Kyle Reicks <reicks.kyle@gmail.com>
2014-08-03 21:00:01 -0500

    Add docblocks to Picturefill_WP

--- inc/class-picturefill-wp.php
+++ inc/class-picturefill-wp.php
@@ -1,19 +1,73 @@
 <?php
+/**
+ * File that defines the controller class for the plugin.
+ *
+ * @since      1.1.1
+ *
+ * @package    Picturefill_WP
+ * @subpackage Picturefill_WP/inc
+ */
+
 defined('ABSPATH') OR exit;
+
 if(!class_exists('Picturefill_WP')){
+
+  /**
+   * Controller class for Picturefill.WP.
+   *
+   * Sets up the plugin and filters the_content.
+   *
+   * @since      1.1.1
+   * @package    Picturefill_WP
+   * @subpackage Picturefill_WP/inc
+   */
   class Picturefill_WP{
 
+    /**
+     * The instance of this class.
+     *
+     * @since  1.1.1
+     * @access private
+     * @var    class Picturefill_WP
+     */
     private static $instance;
+
+    /**
+     * Stores the WordPress global $wpdb.
+     *
+     * @since  2.0.0
+     * @access private
+     * @var    class $wpdb
+     */
     public static $wpdb;
+
+    /**
+     * The application model.
+     *
+     * Sets up data that will be used across the application.
+     *
+     * @since  2.0.0
+     * @access private
+     * @var    class Model_Application_Picturefill_WP
+     */
     private $model;
 
-    // Setup singleton pattern
+    /**
+     * Initialize a single instance of this class.
+     *
+     * @since 2.0.0
+     */
     public static function init(){
       if(null === self::$instance){
         self::$instance = new self();
       }
     }
 
+    /**
+     * Get the instance of this class.
+     *
+     * @since 1.1.1
+     */
     public static function get_instance(){
       self::init();
       return self::$instance;
@@ -27,15 +81,30 @@ if(!class_exists('Picturefill_WP')){
       return null;
     }
 
+    /**
+     * Set the local instance or $wpdb from the global instance of $wpdb.
+     *
+     * @since 2.0.0
+     */
     public static function set_wpdb(){
       global $wpdb;
       self::$wpdb = $wpdb;
     }
 
+    /**
+     * Clear all plugin options on deactivation.
+     *
+     * @since 1.1.3
+     */
     public static function deactivate(){
       self::picturefill_wp_clear_options();
     }
 
+    /**
+     * Clear all plugin options.
+     *
+     * @since 1.1.3
+     */
     public static function picturefill_wp_clear_options(){
       $picturefill_wp_transients = self::$wpdb->get_col('SELECT option_name FROM ' . self::$wpdb->options . ' WHERE option_name LIKE \'%picturefill_wp%\'');
       foreach($picturefill_wp_transients as $transient){
@@ -43,6 +112,11 @@ if(!class_exists('Picturefill_WP')){
       }
     }
 
+    /**
+     * Clear all plugin transients.
+     *
+     * @since 1.1.3
+     */
     public static function picturefill_wp_clear_transients(){
       $picturefill_wp_transients = self::$wpdb->get_col('SELECT option_name FROM ' . self::$wpdb->options . ' WHERE option_name LIKE \'%_picturefill_wp%\'');
       foreach($picturefill_wp_transients as $transient){
@@ -50,23 +124,47 @@ if(!class_exists('Picturefill_WP')){
       }
     }
 
-    // Constructor, add actions and filters
+    /**
+     * Initialize the default functionality of the plugin.
+     *
+     * On init, set the local instance of $wpdb and run the add_update_hook method.
+     * On wp_loaded, set up the application model.
+     * On wp_enqueue_scripts register picturefill.js.
+     * On picturefill_wp_updated, clear all plugin transients.
+     * Filter the_content with picturefill_wp_apply_to_html.
+     *
+     * @since  1.1.1
+     * @access private
+     */
     private function __construct(){
       add_action('init', array('Picturefill_WP', 'set_wpdb'));
       add_action('init', array($this, 'add_update_hook'));
-      add_action('wp_loaded', array($this, 'set_parent_model'));
+      add_action('wp_loaded', array($this, 'set_application_model'));
       add_action('wp_enqueue_scripts', array($this, 'register_picturefill_scripts'));
       add_filter('the_content', array($this, 'picturefill_wp_apply_to_html'), apply_filters('picturefill_wp_the_content_filter_priority', 11));
       add_action('picturefill_wp_updated', array('Picturefill_WP', 'picturefill_wp_clear_transients'));
     }
 
-    // Filter and action methods
-    public function set_parent_model(){
+    /**
+     * Include and instantiate the application model.
+     *
+     * Do action picturefill_wp_register_srcset
+     *
+     * @since 2.0.0
+     */
+    public function set_application_model(){
       require_once(PICTUREFILL_WP_PATH . 'inc/class-model-application-picturefill-wp.php');
       $this->model = new Model_Application_Picturefill_WP();
       do_action('picturefill_wp_register_srcset');
     }
 
+    /**
+     * Register picturefill.js.
+     *
+     * Register the minified script if not in debug mode.
+     *
+     * @since 1.1.1
+     */
     public function register_picturefill_scripts(){
       if(WP_DEBUG){
         wp_register_script('picturefill', PICTUREFILL_WP_URL . 'js/libs/picturefill.js', array(), PICTUREFILL_JS_VERSION, true);
@@ -75,6 +173,15 @@ if(!class_exists('Picturefill_WP')){
       }
     }
 
+    /**
+     * Replace the images in the passed HTML with processed img tags
+     * that include srcset and sizes attributes.
+     *
+     * @since  2.0.0
+     * @param  string $html An HTML string
+     * @param  bool   $cache By default defers to WP_DEBUG
+     * @return string The processed HTML
+     */
     public function picturefill_wp_apply_to_html($html, $cache = null){
       if(!isset($cache)){
         $cache = !WP_DEBUG;
@@ -87,6 +194,13 @@ if(!class_exists('Picturefill_WP')){
       }
     }
 
+    /**
+     * Return the cached output for the passed HTML if it is available.
+     *
+     * @since  1.1.3
+     * @param  string $html An HTML string
+     * @return string The cached output if it is available, the processed HTML if not
+     */
     public function cache_picturefill_output($html){
       $html_hash = md5($html);
       $cache_duration = apply_filters('picturefill_wp_cache_duration', 86400);
@@ -103,6 +217,14 @@ if(!class_exists('Picturefill_WP')){
       }
     }
 
+    /**
+     * Replace the img tags in the passed HTML with img tags
+     * that include srcset and sizes.
+     *
+     * @since  1.1.1
+     * @param  string $html An HTML string
+     * @return string The processed HTML
+     */
     public function replace_images($html){
       do_action('picturefill_wp_before_replace_images');
       require_once(PICTUREFILL_WP_PATH . 'inc/class-model-image-picturefill-wp.php');
@@ -129,6 +251,15 @@ if(!class_exists('Picturefill_WP')){
       return apply_filters('picturefill_wp_replace_images_output', $html);
     }
 
+    /**
+     * Register srcset
+     *
+     * @since  2.0.0
+     * @param  string       $handle Unique identifier
+     * @param  array        $srcset_array Array of URLs that make up the srcset
+     * @param  string|array The image or sizes size to which the srcset should be applied
+     * @return true on success
+     */
     public function register_srcset($handle, $srcset_array, $attached = array()){
       if(is_string($attached)){
         return $this->model->register_srcset($handle, $srcset_array, array($attached));
@@ -136,6 +267,15 @@ if(!class_exists('Picturefill_WP')){
       return $this->model->register_srcset($handle, $srcset_array, $attached);
     }
 
+    /**
+     * Register sizes
+     *
+     * @since  2.0.0
+     * @param  string       $handle Unique identifier
+     * @param  string       $sizes_string A string for the sizes attribute
+     * @param  string|array The image or sizes size to which the sizes attribute should be applied
+     * @return true on success
+     */
     public function register_sizes($handle, $sizes_string, $attached = array()){
       if(is_string($attached)){
         return $this->model->register_sizes($handle, $sizes_string, array($attached));
@@ -143,6 +283,11 @@ if(!class_exists('Picturefill_WP')){
       return $this->model->register_sizes($handle, $sizes_string, $attached);
     }
 
+    /**
+     * Add an action to be fired when the plugin is updated.
+     *
+     * @since 1.1.3
+     */
     public function add_update_hook(){
       if(get_option('picturefill_wp_version') !== PICTUREFILL_WP_VERSION){
         do_action('picturefill_wp_updated');
